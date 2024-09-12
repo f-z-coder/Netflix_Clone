e26d98bbe0b523cea3b205afa4be628578b1f5fe
Faiz Shaikh <faizshaikh72740@gmail.com>
2024-09-12 22:17:25 +0530

    change

--- index.js
+++ index.js
@@ -5,11 +5,11 @@ q.forEach((ques,n)=>{
     ques.addEventListener("click", function() {
         if(openans!=-1){
             ans[openans].style.display='none';
-            q[openans].children[1].setAttribute("src","imgs\\plus.png")
+            q[openans].children[1].setAttribute("src","/imgs/plus.png")
         }
         if (ans[n].style.display ==='none'&&n!=openans) {
             ans[n].style.display = 'block';
-            q[n].children[1].setAttribute("src","imgs\\close.png")
+            q[n].children[1].setAttribute("src","/imgs/close.png")
             openans=n;
           } else {
            openans=-1;
